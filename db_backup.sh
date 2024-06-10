ee3c2f511563ff2335fe78ba2139d352f1962107
rhtshahi <imshahirohit@gmail.com>
2024-06-10 12:54:55 +0545

    Backup Script

---
+++ db_backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# For .env
+source .env
+
+# Get the date of yesterday
+# YESTERDAY=$(date -d "yesterday" +"%Y-%m-%d")
+
+# Current Date
+DATE=$(date +"%Y-%m-%d")
+
+# Connecting with SFTP
+sftp $USER_NAME@$HOST<<EOF
+
+# Navigating to the backup directory 
+cd $SERVER_BACKUP_PATH
+
+# Navigating to the local path to save the backup
+lcd $LOCAL_BACKUP_PATH
+
+# Copy the file to your local device
+get mysql_backup_$DATE*.zip
+
+# Exit SFTP session
+exit
+EOF
